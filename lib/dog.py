61436f6b5e0e32990ca129900152f949d429f299
Teddy Economou <teddyecon@gmail.com>
2023-02-22 20:34:30 -0500

    completed

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if (type(name) == str) and (1 <= len(name) <= 25):
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    
+    def get_breed(self):
+        return self._breed
+    
+    def set_breed(self, breed):
+        approved_breeds = ["Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle", "French Bulldog", "Pug", "Pointer"]
+        if breed in approved_breeds:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+    
+
+
+            
+
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
